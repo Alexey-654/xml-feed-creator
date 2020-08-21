9b30e10e906466cc8533b5e46097d5ff8e5c8592
Alexey-654 <vianosenko@gmail.com>
2020-08-21 15:29:27 +0300

    add test

--- tests/FeedCreatorTest.php
+++ tests/FeedCreatorTest.php
@@ -10,8 +10,14 @@ class FeedCreatorTest extends TestCase
 {
     public function testFeedCreator()
     {
-        // $pathToExpectedFile = __DIR__ . '/../tests/fixtures/feed.xml';
-        // $expected = file_get_contents($pathToExpectedFile, false, null, 3);
-        $this->assertEquals('test', 'test');
+        $pathToExpectedFile = __DIR__ . '/../tests/fixtures/expectedfeed.xml';
+        $expectedData = file_get_contents($pathToExpectedFile, false, null, 3);
+        $creationDate = '2020-06-21T00:00:00+03:00';
+        $pathToInputFile = __DIR__ . '/../tests/fixtures/InputData.xlsx';
+        $pathToOutputFile = __DIR__ . '/../tests/fixtures/feed.xml';
+        $createFeedResult = createFeed($pathToInputFile, $pathToOutputFile, $creationDate);
+        $resultData = file_get_contents($pathToExpectedFile, false, null, 3);
+
+        $this->assertEquals($expectedData, $resultData);
     }
 }
